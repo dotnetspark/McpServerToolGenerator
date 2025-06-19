2a223b5641a8d23747d1a9c9c513ca71dc054366
Yadel Lopez <63487874+dotnetspark@users.noreply.github.com>
2025-06-19 02:23:56 -0400

    #34 refactoring (#35)
    resolves #34

--- samples/CalculatorConsole/Program.cs
+++ samples/CalculatorConsole/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ModelContextProtocol.Server;
 using System.ComponentModel;
+using CalculatorConsole;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Logging.AddConsole(consoleLogOptions =>
@@ -16,4 +17,5 @@ builder.Services
     .WithToolsFromAssembly();
 
 builder.Services.AddSingleton<CalculatorService>();
+
 await builder.Build().RunAsync();
